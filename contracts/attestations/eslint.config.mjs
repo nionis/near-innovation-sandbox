19923b69a6d0e1a52e971af07a6aad37922d8d4a
nioni <stivennoni77@gmail.com>
2026-02-02 10:13:27 +0200

    use node v22

--- contracts/attestations/eslint.config.mjs
+++ contracts/attestations/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from '@repo/eslint-config/base';
+import { config } from '@repo/packages-eslint-config/base';
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
